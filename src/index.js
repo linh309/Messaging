0b1b0a0a122d87ac23a3c7f50c86c5fc437f929f
Van Pham <van.pham@niteco.se>
2019-04-03 16:23:24 +0700

    demo how to display time in real time by calling ReactDom.render every 1 second

--- src/index.js
+++ src/index.js
@@ -13,23 +13,38 @@ import ReactDOM from 'react-dom';
 // serviceWorker.unregister();
 
 
-var user = {
-    name: "Linh",
-    avatar: "https://via.placeholder.com/50x50.png?text=Test",
-};
-var date = new Date();
-var now = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-const name = "linh";
-const h1Tag = <h1>Hello {name}!. Now is {now}</h1>;
-var childAndParent = (
-    <div>
-        <h1>
-            <img src={user.avatar} />
-            Good Morning
-            <b>{user.name}</b>            
-        </h1>        
-    </div>
-);
+// var user = {
 
 
-ReactDOM.render(childAndParent, document.getElementById('root'));
+//     name: "Linh",
+//     avatar: "https://via.placeholder.com/50x50.png?text=Test",
+// };
+// var date = new Date();
+// var now = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+// const name = "linh";
+// const h1Tag = <h1>Hello {name}!. Now is {now}</h1>;
+// var childAndParent = (
+//     <div>
+//         <h1>
+//             <img src={user.avatar} />
+//             Good Morning
+//             <b>{user.name}</b>            
+//         </h1>        
+//     </div>
+// );
+
+
+// ReactDOM.render(childAndParent, document.getElementById('root'));
+
+//demo how to display time in real time by calling ReactDom.render every 1 second
+function tick(){
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>Now is {new Date().toLocaleTimeString()}</h2>
+        </div>        
+    );
+    ReactDOM.render(element, document.getElementById('root'))
+}
+
+setInterval(tick,1000);
