4c7d17cebc8f612136bf284dc5b71ee4efce6008
Van Pham <van.pham@niteco.se>
2019-04-03 15:33:07 +0700

    add code for index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+var user = {
+    name: "Linh",
+    avatar: "https://via.placeholder.com/50x50.png?text=Test",
+};
+var date = new Date();
+var now = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+const name = "linh";
+const h1Tag = <h1>Hello {name}!. Now is {now}</h1>;
+var childAndParent = (
+    <div>
+        <h1>
+            <img src={user.avatar} />
+            Good Morning
+            <b>{user.name}</b>            
+        </h1>        
+    </div>
+);
+
+
+ReactDOM.render(childAndParent, document.getElementById('root'));
