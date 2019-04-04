48b2d83b03a262155a40177852c26d4c509a4d10
Van Pham <van.pham@niteco.se>
2019-04-04 14:05:41 +0700

    check out code

--- src/index.js
+++ src/index.js
@@ -37,14 +37,62 @@ import ReactDOM from 'react-dom';
 // ReactDOM.render(childAndParent, document.getElementById('root'));
 
 //demo how to display time in real time by calling ReactDom.render every 1 second
-function tick(){
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>Now is {new Date().toLocaleTimeString()}</h2>
-        </div>        
-    );
-    ReactDOM.render(element, document.getElementById('root'))
+// function tick(){        
+//     const element = (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>Now is {new Date().toLocaleTimeString()}</h2>
+//         </div>        
+//     );
+//     ReactDOM.render(element, document.getElementById('root'))
+// }
+
+// setInterval(tick,1000);
+
+
+//define react component as class ES6 syntax
+var user = {
+    name: "Linh",
+    avatar: "https://via.placeholder.com/50x50.png?text=Test",
+};
+
+class Welcome extends React.Component {
+    //use in normal case
+    // //define constructor to get props that passed from ReactDOM.render method
+    // constructor(props) {
+    //     super(props);
+    // }
+    
+    // render() {
+    //     //why need to use this to use "props" => can't use props without "this"
+    //     return <h1>Hello, {this.props.user.name} again!</h1>
+    // }
+
+    //define constructor to get props that passed from ReactDOM.render method
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <User username={this.props.user.name}/>
+            </div>        
+        )
+    }
 }
 
-setInterval(tick,1000);
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        //why need to use this to use "props" => can't use props without "this"
+        return <h1>Hello, {this.props.username} again!</h1>
+    }
+}
+
+
+//use "user" as DOM attribute but it's treated props in component
+ReactDOM.render(<Welcome user={user} />, document.getElementById("root"));
